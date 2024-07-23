2bbc1274ba7eda1929f1a07c817ee9c39ef32a49
JWillC3 <wclagett@colostate.edu>
2024-07-23 16:20:01 -0600

    "finalized" template

--- r files/TEMPLATE.R
+++ r files/TEMPLATE.R
@@ -3,71 +3,71 @@ source("source_data.R")
 
 #replace the site number and name first
 #change the dates for plots "Oct. 24 - Oct. 31, 2023."
-#change the site location "Teaching Tree (Post)" and Fort Collins"
-#change [location info] for where the canisters were located
-#for plot colors: Location 1: "orchid", Location 2: "chocolate4",
-#Location 3: "goldenrod2",Location 4: "#50C878",
-#Location 5: "tomato2", Location 6: "midnightblue"
+#change the site location "[Site Name] " 
+#location1, location2, ect. for objects and plots
+#location_1, location_2, ect. for name in plot titles or axis
+#change "[floor] [room type]" for where the canisters were located
+
 
 #calculate ratios
-indoor_040 <- indoor_040 %>%
+indoor_XXX <- indoor_XXX %>%
   group_by(room_name, analyte) %>%
   ungroup() %>% 
-  mutate(od_ratio = (indoor_040$conc./outdoor_040$conc.))
+  mutate(od_ratio = (indoor_XXX$conc./outdoor_XXX$conc.))
 
 #UPDATE LOCATIONS FOR THE SITE YOU ARE CURRENTLY WORKING ON, Location 1, 2, etc.!
 #location order should be alphabetical
 
-#bears
-bears <- site_040 %>% 
-  filter(room_name == "Bears")
-#frogs
-frogs <- site_040 %>% 
-  filter(room_name == "Frogs")
-#lesson prep
-lesson_prep <- site_040 %>% 
-  filter(room_name == "Lesson Prep")
-#monkeys
-monkeys <- site_040 %>% 
-  filter(room_name == "Monkeys")
-#office
-office <- site_040 %>% 
-  filter(room_name == "Office")
+#location1
+location1 <- site_XXX %>% 
+  filter(room_name == "location_1")
+#location2
+location2 <- site_XXX %>% 
+  filter(room_name == "location_2")
+#location3
+location3 <- site_XXX %>% 
+  filter(room_name == "location_3")
+#location4
+location4 <- site_XXX %>% 
+  filter(room_name == "location_4")
+#location5
+location5 <- site_XXX %>% 
+  filter(room_name == "location_5")
 
 #data table
-site_040_table <- data_table(sites, "040")
+site_XXX_table <- data_table(sites, "XXX")
 
-site_dt(site_040_table, "040")
+site_dt(site_XXX_table, "XXX")
 
 #box plot for all analytes
-bp_040 <- box_plot(site_040)
-bp_040
+bp_XXX <- box_plot(site_XXX)
+bp_XXX
 
 #plot all VOCs at site
-p_040 <- p_site(site_040, "040")
-p_040
+p_XXX <- p_site(site_XXX, "XXX")
+p_XXX
 
 #plotly output but y axis not formatted correctly and don't know how to fix
-ggplotly(p_040, tooltip = "text")
+ggplotly(p_XXX, tooltip = "text")
 
 #top 10 analyte concentrations for all locations
-site_040_top <- top_n_analytes(site_040, n = 45)
+site_XXX_top <- top_n_analytes(site_XXX, 45)
 
-site_040_top <- top_plot(site_040_top, "040", fill = "blue")
-site_040_top
+site_XXX_top <- top_plot(site_XXX_top, "XXX", fill = "blue")
+site_XXX_top
 
 #facet wrap by location
-p_040_fctw <- fct_wrap(site_040, "040")
-p_040_fctw 
+p_XXX_fctw <- fct_wrap(site_XXX, "XXX")
+p_XXX_fctw 
 
 #leave this out for now
 #Two plots together not joined
-# p_040 +
-#   p_040_fctw +
+# p_XXX +
+#   p_XXX_fctw +
 #   plot_layout(nrow = 2, heights = c(1, 2))
 
 #facet wrap all analytes grouped by class
-p_040_cat_fctw <- ggplot(site_040, aes(x = reorder(analyte, conc.),
+p_XXX_cat_fctw <- ggplot(site_XXX, aes(x = reorder(analyte, conc.),
                                        y = conc.)) +
   geom_point(color = "orange", size = 3, shape = 18, alpha = 0.5) +
   xlab("Analytes") +
@@ -79,89 +79,87 @@ p_040_cat_fctw <- ggplot(site_040, aes(x = reorder(analyte, conc.),
   labs(x = "Analytes",
        y = expression(atop("Concentration",
                            paste("(VOC ppbv or methane ppmv)")))) +
-  ggtitle("Site 040 - Teaching Tree (Post), Summa Cannister Deployment",
+  ggtitle("Site XXX - [Site Name] , Summa Cannister Deployment",
           "Grouped by Analyte Category")
-p_040_cat_fctw
+p_XXX_cat_fctw
 
 #indoor facet wrap by analyte class
-p_040_cat_fctw <- cat_fct_wrap(indoor_040, "040")
-p_040_cat_fctw
+p_XXX_cat_fctw <- cat_fct_wrap(indoor_XXX, "XXX")
+p_XXX_cat_fctw
 
 #plots for each room
 #outdoor
-p_040od <- room_plot(outdoor_040, "040", "midnightblue", "Outdoor, rooftop")
+p_XXXooutdoor <- room_plot(outdoor_XXX, "XXX", "midnightblue", "Outdoor, rooftop")
 
-p_040od
+p_XXXoutdoor
 
-#bears
-p_bears <- room_plot(bears, "040", "orchid", "Bears, first floor classroom")
-p_bears
+#location1
+p_location1 <- room_plot(location1, "XXX", "orchid", "location_1, [floor] [room type]")
+p_location1
 
-#frogs
-p_frogs <- room_plot(frogs, "040", "chocolate4", "Frogs, first floor classroom")
-p_frogs
+#location2
+p_location2 <- room_plot(location2, "XXX", "chocolate4", "location_2, [floor] [room type]")
+p_location2
 
-#lesson_prep
-p_lesson_prep <- room_plot(lesson_prep, "040", "goldenrod2",
-                           "Lesson Prep, second floor office")
-p_lesson_prep
+#location3
+p_location3 <- room_plot(location3, "XXX", "goldenrod2", "location_3, [floor] [room type]")
+p_location3
 
-#monkeys
-p_monkeys <- room_plot(monkeys, "040", "#50c878",
-                       "Monkeys, first floor classroom")
-p_monkeys
+#location4
+p_location4 <- room_plot(location4, "XXX", "#50c878", "location_4, [floor] [room type]")
+p_location4
 
-#office
-p_office <- room_plot(office, "040", "tomato2", "Large Office, first floor")
-p_office
+#location5
+p_location5 <- room_plot(location5, "XXX", "tomato2", "location_5, [floor] [room type]")
+p_location5
 
 #all rooms
-grid.arrange(p_bears, p_frogs, p_monkeys, p_lesson_prep, p_office, p_040od, 
+grid.arrange(p_location1, p_location2, p_location4, p_location3, p_location5, p_XXXod, 
              ncol = 3,
              bottom = "Rooms Sampled", left = "Sum of VOC Sampled (ppb(v))")
 
 
 #create object of the top 5 analytes by conc. for the indoor locations then plot
-#bears
+#location1
 #testing something new for scale y in this plot
-bears_top <- top_n_analytes(bears, n = 10)
+location1_top <- top_n_analytes(location1, 10)
 
-p_bears_top <- loc_top_plot(bears_top, "orchid", "Bears")
-p_bears_top
+p_location1_top <- loc_top_plot(location1_top, "orchid", "location_1")
+p_location1_top
 
-#frogs
-frogs_top <- top_n_analytes(frogs, n = 10)
+#location2
+location2_top <- top_n_analytes(location2, 10)
 
-p_frogs_top <- loc_top_plot(frogs_top, "chocolate4", "Frogs")
-p_frogs_top
+p_location2_top <- loc_top_plot(location2_top, "chocolate4", "location_2")
+p_location2_top
 
-#lesson_prep
-lesson_prep_top <- top_n_analytes(lesson_prep, n = 10)
+#location3
+location3_top <- top_n_analytes(location3, 10)
 
-p_lesson_prep_top <- loc_top_plot(lesson_prep_top, "goldenrod2", "Lesson Prep")
-p_lesson_prep_top
+p_location3_top <- loc_top_plot(location3_top, "goldenrod2", "location_3")
+p_location3_top
 
-#monkeys
-monkeys_top <- top_n_analytes(monkeys, n = 10)
+#location4
+location4_top <- top_n_analytes(location4, 10)
 
-p_monkeys_top <- loc_top_plot(monkeys_top, "#50c878", "Monkeys")
-p_monkeys_top
+p_location4_top <- loc_top_plot(location4_top, "#50c878", "location_4")
+p_location4_top
 
-#office
-office_top <- top_n_analytes(office, n = 10)
+#location5
+location5_top <- top_n_analytes(location5, 10)
 
-p_office_top <- loc_top_plot(office_top, "tomato2", "Office")
-p_office_top
+p_location5_top <- loc_top_plot(location5_top, "tomato2", "location_5")
+p_location5_top
 
 #outdoor
-outdoor_top <- top_n_analytes(outdoor_040, n = 10)
+outdoor_top <- top_n_analytes(outdoor_XXX, 10)
 
 p_outdoor_top <- loc_top_plot(outdoor_top, "midnightblue", "Outdoor")
 p_outdoor_top
 
 #all locations
-grid.arrange(p_bears_top, p_frogs_top, p_lesson_prep_top,
-             p_monkeys_top, p_office_top, p_outdoor_top,
+grid.arrange(p_location1_top, p_location2_top, p_location3_top,
+             p_location4_top, p_location5_top, p_outdoor_top,
              ncol = 3, nrow = 2,
              top = "Top 10 Analytes at Each Location",
              left = "Concentration\n(VOC ppbv or methane ppmv)")
@@ -169,54 +167,54 @@ grid.arrange(p_bears_top, p_frogs_top, p_lesson_prep_top,
 #indoor, outdoor ratio 
 
 #boxplot of indoor / outdoor ratios for all analytes
-p_040_bp <- r_box_plot(indoor_040)
-p_040_bp
+p_XXX_bp <- r_box_plot(indoor_XXX)
+p_XXX_bp
 
 #indoor to outdoor ratio plot for all analytes
-p_040_ratio <- r_p_site(indoor_040, "040")
-p_040_ratio
+p_XXX_ratio <- r_p_site(indoor_XXX, "XXX")
+p_XXX_ratio
 
 #plotly ratio
-ggplotly(p_040_ratio, tooltip = "text")
+ggplotly(p_XXX_ratio, tooltip = "text")
 
 #ratio plot with facet wrap
-p_040_r_fctw <- r_fct_wrap(indoor_040, "040")
-p_040_r_fctw
+p_XXX_r_fctw <- r_fct_wrap(indoor_XXX, "XXX")
+p_XXX_r_fctw
 
-#create object of the top 5 analytes by outdoor ratio then plot
-#bears outdoor ratio
-bears_top_or <- top_n_or(indoor_040, "Bears", 10)
+#create object of the top 10 analytes by outdoor ratio then plot
+#location1 outdoor ratio
+location1_top_or <- top_n_or(indoor_XXX, "location_1", 10)
 
-p_bears_top_or <- or_top_plot(bears_top_or, "orchid", "Bears")
-p_bears_top_or
+p_location1_top_or <- or_top_plot(location1_top_or, "orchid", "location_1")
+p_location1_top_or
 
-#frogs outdoor ratio
-frogs_top_or <- top_n_or(indoor_040, "Bears", 10)
+#location2 outdoor ratio
+location2_top_or <- top_n_or(indoor_XXX, "location_1", 10)
 
-p_frogs_top_or <- or_top_plot(frogs_top_or, "chocolate4", "Frogs")
-p_frogs_top_or
+p_location2_top_or <- or_top_plot(location2_top_or, "chocolate4", "location_2")
+p_location2_top_or
 
-#lesson_prep outdoor ratio
-lesson_prep_top_or <- top_n_or(indoor_040, "Lesson Prep", 10)
+#location3 outdoor ratio
+location3_top_or <- top_n_or(indoor_XXX, "location_3", 10)
 
-p_lesson_prep_top_or <- or_top_plot(lesson_prep_or, "goldenrod2", "Lesson Prep")
-p_lesson_prep_top_or
+p_location3_top_or <- or_top_plot(location3_or, "goldenrod2", "location_3")
+p_location3_top_or
 
-#monkeys outdoor ratio
-monkeys_top_or <- top_n_or(indoor_040, "Monkeys", 10)
+#location4 outdoor ratio
+location4_top_or <- top_n_or(indoor_XXX, "location_4", 10)
 
-p_monkeys_top_or <- or_top_plot(monkeys_top_or, "#50C878", "Monkeys")
-p_monkeys_top_or
+p_location4_top_or <- or_top_plot(location4_top_or, "#50C878", "location_4")
+p_location4_top_or
 
-#office outdoor ratio
-office_top_or <- top_n_or(indoor_040, "Office", 10)
+#location5 outdoor ratio
+location5_top_or <- top_n_or(indoor_XXX, "location_5", 10)
 
-p_office_top_or <- or_top_plot(office_top_or, "tomato2", "Office")
-p_office_top_or
+p_location5_top_or <- or_top_plot(location5_top_or, "tomato2", "location_5")
+p_location5_top_or
 
 #all locations outdoor ratio
-grid.arrange(p_bears_top_or, p_frogs_top_or, p_lesson_prep_top_or,
-             p_monkeys_top_or, p_office_top_or,
+grid.arrange(p_location1_top_or, p_location2_top_or, p_location3_top_or,
+             p_location4_top_or, p_location5_top_or,
              ncol = 3, nrow = 2,
              top = "Top 10 I/O Ratios in Each Location",
              left = "Concentration\n(VOC ppbv or methane ppmv)")
@@ -263,30 +261,30 @@ grid.arrange(p_btex, p_chlorinated, p_ketone, p_other, ncol = 2, nrow = 2,
              top = "", left = "Concentraion\n (VOC ppbv or methane ppmv")
 
 # #correlations
-cor(frogs$conc., bears$conc., method = "spearman")
+cor(location2$conc., location1$conc., method = "spearman")
 #repeat the above for all location you want correlation for
 #create a new df for correlation. Slice out "outdoor" col. num. [-X]
-voc_cor <- site_040 %>%
+voc_cor <- site_XXX %>%
   slice(1:2)
 
-#get all TVOC signals for each room, ex. "sum(office$conc., na.rm = TRUE)"
+#get all TVOC signals for each room, ex. "sum(location5$conc., na.rm = TRUE)"
 p.analytes$conc. <- as.numeric(as.character(p.analytes$conc.))
 
-tvoc_bears <- p.analytes %>%
-  filter(ID == "Bears")
-sum(tvoc_bears$conc.)
-tvoc_frogs <- p.analytes %>% 
-  filter(ID == "Frogs")
-sum(tvoc_frogs$conc., na.rm = TRUE)
+tvoc_location1 <- p.analytes %>%
+  filter(ID == "location_1")
+sum(tvoc_location1$conc.)
+tvoc_location2 <- p.analytes %>% 
+  filter(ID == "location_2")
+sum(tvoc_location2$conc., na.rm = TRUE)
 tvoc_lessonprep <- p.analytes %>% 
-  filter(ID == "Lesson Prep")
+  filter(ID == "location_3")
 sum(tvoc_lessonprep$conc., na.rm = TRUE)  
-tvoc_monkeys <- p.analytes %>% 
-  filter(ID == "Monkeys")
-sum(tvoc_monkeys$conc., na.rm = TRUE)
-tvoc_office <- p.analytes %>% 
-  filter(ID == "Office")
-sum(tvoc_office$conc., na.rm = TRUE)
+tvoc_location4 <- p.analytes %>% 
+  filter(ID == "location_4")
+sum(tvoc_location4$conc., na.rm = TRUE)
+tvoc_location5 <- p.analytes %>% 
+  filter(ID == "location_5")
+sum(tvoc_location5$conc., na.rm = TRUE)
 tvoc_outdoor <- p.analytes %>% 
   filter(ID == "Outdoor")
 sum(tvoc_outdoor$conc., na.rm = TRUE)
